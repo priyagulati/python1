17c5acd33561eec8246c4fdcb36b333610d4f77e
Priyanka <prpgrover@gmail.com>
2018-07-02 20:56:17 -0700

    By Priyanka

---
+++ Day1_cw.py
@@ -0,0 +1,24 @@
+# How to use lower,tittle,upper methods
+# Use of + oprator for concatention
+# Use of " " for giving space
+# Method always begins with .method name())
+
+name="priyanka gulati"
+print(name.upper())
+
+first_name="priyanka"
+last_name="gulati"
+full_name=first_name+ " " +last_name
+message="Hello,"+full_name.title()+"!"
+print(message)
+
+#my_favorite_car=" Toyota "
+#print(my_favorite_car.rstrip())
+#print(my_favorite_car.lstrip())
+
+#my_favorite_car=my_favorite_car.rstrip()
+#print(my_favorite_car)
+
+#age=23
+#message="Happy"+ (age)+ "rd Birthday!"
+#print(message)
